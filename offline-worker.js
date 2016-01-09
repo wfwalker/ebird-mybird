9b29a1bce80c7480e67e6bac397e3b009d2713a5
Travis CI <bwalker@mozilla.com>
2016-01-09 18:35:26 +0000

    Updates

--- offline-worker.js
+++ offline-worker.js
@@ -37,7 +37,7 @@
 
     // This is the unique name for the cache controlled by this version of the worker.
     get CACHE_NAME() {
-      return this.CACHE_PREFIX + '8aeed0f5a3c832b1cafb46cda2fee9a168d39891';
+      return this.CACHE_PREFIX + '9db617473bbde45554c3bc2cf780e5d4ab8bc95d';
     },
 
     // This is a list of resources that will be cached.
@@ -51,7 +51,7 @@
       './images/icon_download.png', // 166867841b1cd0c08246cc00a16f36b8777f0fff
       './images/sprite_download.png', // 6e7e6d4aafb6b0f526feb0369038efd8c8f1fb6d
       './index.html', // 2026a58e64ffeb98be0acc05593828a863efc88f
-      './scripts/compressed.js', // a4230f961ada301fff9641b1d4af2ff6c0d4528b
+      './scripts/compressed.js', // 5ebb9703c7baca6d8915a35c5935e368b2f1050a
       './data/day-names.json', // b3f7ae075cba79adddd0d413a0138c8a273e020a
       './data/ebird.csv', // 69a5aacea861887696adb297f3fdfa108a7815a6
       './data/omitted-common-names.json', // 2a2dfcf3b8c01de450738adc77352c1b53d97081
