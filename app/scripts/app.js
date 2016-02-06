64a6620916d8cb7a7bed19c4a47c634e89c87346
Bill Walker <bwalker@mozilla.com>
2016-02-05 17:41:47 -0800

    fix lint warnings

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,7 @@ Date.prototype.getWeek = function() {
   var week1 = new Date(date.getFullYear(), 0, 4);
   // Adjust to Thursday in week 1 and count number of weeks from date to week1.
   return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
-}
+};
 
 var gSightings = null;
 var gOmittedCommonNames = [];
