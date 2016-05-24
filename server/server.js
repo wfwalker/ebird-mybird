7f89cad59e2e4801dda4f72eea392b0f88a139ad
Bill Walker <walker@shout.net>
2016-05-23 23:57:06 -0700

    do home page (photos of the week) from server

--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ app.use("/", express.static('dist'));
 // parse the ebird data so we can make a REST API for it
 
 var gSightings = [];
+var gPhotos = [];
 
 fs.readFile('app/data/ebird.csv', 'utf8', function(err, data) {
 	if (err) throw err;
@@ -28,7 +29,47 @@ fs.readFile('app/data/ebird.csv', 'utf8', function(err, data) {
 	gSightings = ebird.data;
 });
 
+fs.readFile('app/data/photos.json', 'utf8', function(err, data) {
+	if (err) throw err;
+	gPhotos = JSON.parse(data);
+
+	for (var index = 0; index < gPhotos.length; index++)
+	{
+		var photo = gPhotos[index];
+
+		// set the photos's ID as its index in this array.
+		// TODO: not permanently stable
+		photo.id = index;
+
+		// Parse the date
+		var pieces = photo['Date'].split('-');
+
+		// order the pieces in a sensible way
+		var fixedDateString = [pieces[0], '/', pieces[1], '/', pieces[2]].join('');
+
+		// create and save the new dat
+		var newDate = new Date(fixedDateString);
+		photo['DateObject'] = newDate;
+	}
+});
+
+// ADD getWeek to Date class
+
+Date.prototype.getWeek = function() {
+    var dt = new Date(this.getFullYear(),0,1);
+    return Math.ceil((((this - dt) / 86400000) + dt.getDay()+1)/7);
+};
+
 // REST API for ebird data
+
+app.get('/photosThisWeek', function(req, resp, next) {
+	var currentWeekOfYear = new Date().getWeek();
+	var photosThisWeek = gPhotos.filter(function(p) { return p.DateObject.getWeek() == currentWeekOfYear; });	
+
+	console.log('photos of the week', photosThisWeek.length);
+
+	resp.json(photosThisWeek);
+});
 
 app.get('/locationSightingsTaxonomic/:location_name', function(req, resp, next) {
 	var locationSightingsTaxonomic = gSightings.filter(function(s) { return s['Location'] == req.params.location_name; });
