8a8c6f01fb28d5ac93cfa10f35ee9c523417d850
Travis CI <bwalker@mozilla.com>
2016-06-23 05:53:05 +0000

    Updates

--- offline-worker.js
+++ offline-worker.js
@@ -39,7 +39,7 @@
 
     // This is the unique name for the cache controlled by this version of the worker.
     get CACHE_NAME() {
-      return this.CACHE_PREFIX + '4520947a23bd81d924219d68f925f7f8170ba3fe';
+      return this.CACHE_PREFIX + 'd680cd9631fbf3c0b689ffd296ed2cf7037721ad';
     },
 
     // This is a list of resources that will be cached.
@@ -48,7 +48,7 @@
       './images/ebird-favicon.ico', // 783739b89f16a72a7a0a55720ff6acc34048351c
       './images/ebird-favicon.png', // 3f841e97f2ecb0dc0f16478fa57c9dd00b65300f
       './index.html', // ba1b66f3b197d36db93d65b0754a0fd30887afc6
-      './scripts/compressed.js', // 777b1ce11d405f8c61063bc285309eacc73605aa
+      './scripts/compressed.js', // 6a5ddf13886988c2d6d34c0843e7634918799c51
       './styles/app.css', // bbc88b7bc89a0312ac26cbb1289762850dc0362c
       './styles/bundle.css', // bfe99fc1b82f43ee35c7a3a6f020dadaebb551d2
       './styles/c3.min.css', // 0ed505e8458512b29c9925c83316a48b1b838b10
