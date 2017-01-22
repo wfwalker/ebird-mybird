9920e691bf961401fabd9c0a5fe88218e5a44131
Bill Walker <walker@shout.net>
2017-01-21 18:47:46 -0800

    only bundle up bundle.css remove ununsed JS file name

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,3 @@
-/**
- * Copyright 2015 Mozilla
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 'use strict';
 
 // var connect = require('gulp-connect');
@@ -110,7 +94,7 @@ gulp.task('deploy', function() {
 });
 
 gulp.task('lint', function() {
-  return gulp.src(['app/scripts/sightinglist.js', 'app/scripts/renders.js', 'app/scripts/app.js', 'app/test/test.js']).pipe(eslint({
+  return gulp.src(['app/scripts/sightinglist.js', 'app/scripts/app.js', 'app/test/test.js']).pipe(eslint({
     'rules':{
         'quotes': [1, 'single'],
         'semi': [1, 'always'],
@@ -173,7 +157,7 @@ gulp.task('offline', ['build'], function() {
       'images/**',
       'index.html',
       'scripts/compressed.js',
-      'styles/**',
+      'styles/bundle.css',
       'fonts/**',
     ],
   });
