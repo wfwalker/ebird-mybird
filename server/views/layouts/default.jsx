bdda845e09c8d53ef097fb742deef63b09b4523b
Bill Walker <walker@shout.net>
2017-06-15 08:39:32 -0700

    extract method

--- server/views/layouts/default.jsx
+++ server/views/layouts/default.jsx
@@ -1,9 +1,8 @@
 var React = require('react');
  
 var DefaultLayout = React.createClass({
-  render: function() {
-    return (
-      <html>
+	generateHead: function() {
+		return (
         <head>
 		    <meta charset="utf-8" />
 		    <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes" />
@@ -30,44 +29,57 @@ var DefaultLayout = React.createClass({
 		    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" crossorigin="anonymous"></script>
 		 
     	</head>
+		)
+	},
+
+	generateNav: function() {
+		return (
+	    <nav className="navbar navbar-default navbar-fixed-top">
+	      <div className="container">
+	        <div className="navbar-header">
+	          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	            <span className="sr-only">Toggle navigation</span>
+	            <span className="icon-bar"></span>
+	            <span className="icon-bar"></span>
+	            <span className="icon-bar"></span>
+	          </button>
+	          <a className="navbar-brand" href="/photos">BirdWalker</a>
+	        </div>
+	        <div id="navbar" className="collapse navbar-collapse">
+	          <ul className="nav navbar-nav">
+	            <li><a data-hash="photos" href="/photos">Photos by Bill Walker</a></li>
+	            <li className="dropdown">
+	              <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Birding <span className="caret"></span></a>
+	              <ul className="dropdown-menu">
+	                <li><a href="/bigdays">Big Days</a></li>
+	                <li><a href="/locations">Locations</a></li>
+	                <li><a href="/trips">Trips</a></li>
+	                <li><a href="/chrono">Chronological Life List</a></li>
+	                <li><a href="/taxons">Taxonomic Life List</a></li>
+	              </ul>
+	            </li>
+	          </ul>
+
+	          <form action='/search' method='GET' className="navbar-form navbar-left" role="search">
+	            <div className="form-group">
+	              <input id='searchtext' name='searchtext' type="text" className="form-control" placeholder="Search" />
+	            </div>
+	            <button id='gosearch' type="submit" className="btn btn-default">Submit</button>
+	          </form>
+
+	        </div>
+	      </div>
+	    </nav>
+		)
+	},
+
+  render: function() {
+    return (
+      <html>
+      	{this.generateHead()}
         <body>
 
-		    <nav className="navbar navbar-default navbar-fixed-top">
-		      <div className="container">
-		        <div className="navbar-header">
-		          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-		            <span className="sr-only">Toggle navigation</span>
-		            <span className="icon-bar"></span>
-		            <span className="icon-bar"></span>
-		            <span className="icon-bar"></span>
-		          </button>
-		          <a className="navbar-brand" href="/photos">BirdWalker</a>
-		        </div>
-		        <div id="navbar" className="collapse navbar-collapse">
-		          <ul className="nav navbar-nav">
-		            <li><a data-hash="photos" href="/photos">Photos by Bill Walker</a></li>
-		            <li className="dropdown">
-		              <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Birding <span className="caret"></span></a>
-		              <ul className="dropdown-menu">
-		                <li><a href="/bigdays">Big Days</a></li>
-		                <li><a data-hash="locations" href="/locations">Locations</a></li>
-		                <li><a href="/trips">Trips</a></li>
-		                <li><a href="/chrono">Chronological Life List</a></li>
-		                <li><a href="/taxons">Taxonomic Life List</a></li>
-		              </ul>
-		            </li>
-		          </ul>
-
-		          <form action='/search' method='GET' className="navbar-form navbar-left" role="search">
-		            <div className="form-group">
-		              <input id='searchtext' name='searchtext' type="text" className="form-control" placeholder="Search" />
-		            </div>
-		            <button id='gosearch' type="submit" className="btn btn-default">Submit</button>
-		          </form>
-
-		        </div>
-		      </div>
-		    </nav>
+        	{this.generateNav()}
 
 	        <div className="container" style={{paddingTop: '60px'}}>
         	{this.props.children}
