cc7cc0cb1deb0f3bf6adb4086cdeaac09635e302
Bill Walker <walker@shout.net>
2025-07-13 16:16:47 -0500

    updated name of photo external HD updated CSV parsing library updated date parsing look further back in JPEG’s folder

--- workflow.js
+++ workflow.js
@@ -2,18 +2,24 @@ console.log('hello, world');
 
 var iptc = require('node-iptc');
 var fs = require('fs');
-var babyParse = require('babyparse');
+var papaParse = require('papaparse')
 var glob = require('glob');
 var SightingList = require('./server/scripts/sightinglist.js');
 var XML = require('pixl-xml');
 
 var gSightingList = null;
 
-var csvData = fs.readFileSync('server/data/ebird.csv', 'utf8');
+let fileBytes = fs.readFileSync('server/data/ebird.csv', 'utf8')
 
-var ebird = babyParse.parse(csvData, {
-    header: true,
-});
+if (fileBytes.charCodeAt(0) === 0xFEFF) {
+  fileBytes = fileBytes.slice(1);
+}
+
+var ebird = papaParse.parse(fileBytes, {
+  header: true,
+  dynamicTyping: true
+})
+
 console.log('parsed ebird', ebird.data.length);
 
 var jsonData = fs.readFileSync('server/data/photos.json');
@@ -28,14 +34,14 @@ var newPhotos = []
 
 var consoleCommands = []
 
-function handleXMP(inXMPPath, tmpEbirdDate, n, tmpDate) {
+function handleXMP(inXMPPath, inSeperatedIPTCDate, n) {
     let data = fs.readFileSync(inXMPPath)
 
     let result = XML.parse(data)
 
     let label = result['rdf:RDF']['rdf:Description']['xmp:Label'];
     let location = result['rdf:RDF']['rdf:Description']['Iptc4xmpCore:Location'];
-    let daySightingList = new SightingList(gSightingList.filter(function(s) { return s['Date'] == tmpEbirdDate; }));
+    let daySightingList = new SightingList(gSightingList.filter(function(s) { return s['Date'] == inSeperatedIPTCDate; }));
     let speciesSightings = daySightingList.filter(s => { return s['Common Name'] == label });
     let photosOriginalNameMatch = photos.filter(p => p['Filename'].toLowerCase().indexOf(n.toLowerCase()) > 0);
 
@@ -46,8 +52,8 @@ function handleXMP(inXMPPath, tmpEbirdDate, n, tmpDate) {
     let info = {
         label: label,
         location: location,
-        date: tmpDate,
-        ebirdDate: tmpEbirdDate, 
+        date: inSeperatedIPTCDate,
+        ebirdDate: inSeperatedIPTCDate, 
         sightingsThatDate: daySightingList.length(),
         speciesSightingsThatDate: speciesSightings,
         locations: daySightingList.getUniqueValues('Location'),
@@ -55,15 +61,15 @@ function handleXMP(inXMPPath, tmpEbirdDate, n, tmpDate) {
     }
 
     if (photosOriginalNameMatch.length > 0) {
-        // console.log('already found', n, label, 'in photos.json', tmpEbirdDate, location);
+        // console.log('already found', n, label, 'in photos.json', inSeperatedIPTCDate, location);
         info.action = 'already found in photos.json'
         info.needsAction = false
-        // console.log('already found', n, label, 'in photos.json', tmpEbirdDate, location, photosOriginalNameMatch[0]);
+        // console.log('already found', n, label, 'in photos.json', inSeperatedIPTCDate, location, photosOriginalNameMatch[0]);
     } else if (label == 'No Label') {
         info.action = 'photo lacks species label'
         // console.log(JSON.stringify(result, null, '  '))
     } else if (speciesSightings.length > 0) {
-        let newFilename = tmpDate + '-' + speciesSightings[0]['Scientific Name'].toLowerCase().replace(' ', '-') + '-' + n;
+        let newFilename = inSeperatedIPTCDate + '-' + speciesSightings[0]['Scientific Name'].toLowerCase().replace(/ /g, '-') + '-' + n;
 
         info.action = 'READY to add to photos.json'
 
@@ -83,11 +89,11 @@ function handleXMP(inXMPPath, tmpEbirdDate, n, tmpDate) {
         consoleCommands.push('s3cmd put --acl-public /Users/walker/Photography/flickrUP/' + newFilename + ' s3://birdwalker/photo/ --add-header=Cache-Control:max-age=31536000')
         consoleCommands.push('s3cmd put --acl-public /Users/walker/Photography/flickrUP/' + newFilename + ' s3://birdwalker/thumb/ --add-header=Cache-Control:max-age=31536000')
     } else if (daySightingList.length() > 0) {
-        info.action = 'missing species from existing http://www.birdwalker.com/trip/' + tmpEbirdDate
-        // console.log(n, 'trip yes but', label, 'no', tmpEbirdDate);
+        info.action = 'missing species from existing http://www.birdwalker.com/trip/' + inSeperatedIPTCDate
+        // console.log(n, 'trip yes but', label, 'no', inSeperatedIPTCDate);
     } else {
-        info.action = 'missing trip for date ' + tmpEbirdDate
-        // console.log(n, label, 'no trip this date', tmpEbirdDate);
+        info.action = 'missing trip for date ' + inSeperatedIPTCDate
+        // console.log(n, label, 'no trip this date', inSeperatedIPTCDate);
     }
 
     return info
@@ -96,45 +102,46 @@ function handleXMP(inXMPPath, tmpEbirdDate, n, tmpDate) {
 function handleJPEG(inJPEGFilename) {
     let jpegFilePath = '/Users/walker/Photography/flickrUP/' + inJPEGFilename
     let tmpFile = fs.readFileSync(jpegFilePath);
+    let info = {
+        needsAction: true,
+        label: 'No IPTC Date',
+        name: inJPEGFilename
+    }
 
     // IPTC date is year month day no seperators '20151223'
     let tmpIPTCdate = iptc(tmpFile).date_created;
 
     if (tmpIPTCdate == undefined) {
-        throw 'No IPTC date for ' + inJPEGFilename
+        console.log('No IPTC date for ' + inJPEGFilename)
+        return info
     }
 
     // seperated IPTC date year-month-day like '2015-12-23'    
     let tmpSeperatedIPTCDate = tmpIPTCdate.substring(0,4) + '-' + tmpIPTCdate.substring(4,6) + '-' + tmpIPTCdate.substring(6,8);
 
     // seperated month-day-year like '12-23-2015' as appears in CSV data dump from eBird
-    let tmpEbirdDate = tmpIPTCdate.substring(4,6) + '-' + tmpIPTCdate.substring(6,8) + '-' + tmpIPTCdate.substring(0,4);
+    // let tmpEbirdDate = tmpIPTCdate.substring(4,6) + '-' + tmpIPTCdate.substring(6,8) + '-' + tmpIPTCdate.substring(0,4);
 
     let yearDateFilenameDotXMP = tmpIPTCdate.substring(0,4) + '/' + tmpSeperatedIPTCDate + '/' + inJPEGFilename.replace('jpg', 'xmp');
 
-    let info = {
-        needsAction: true,
-        label: 'Missing XMP'
-    }
-
-    let path1 = '/Volumes/Big\ Ethel/Photos/' + yearDateFilenameDotXMP
+    let path1 = '/Volumes/Batholith/Photos/' + yearDateFilenameDotXMP
     let path2 = '/Users/walker/Pictures/' + yearDateFilenameDotXMP
-    let path3 = '/Volumes/Big Ethel/Photos/' + tmpSeperatedIPTCDate + '/' + inJPEGFilename.replace('jpg','xmp')
-    let path4 = '/Volumes/Big Ethel/Photos/photos-' + tmpSeperatedIPTCDate + '/' + inJPEGFilename.replace('jpg','xmp')
-    let path5 = '/Volumes/Big Ethel/Photos/photos-' + tmpSeperatedIPTCDate + 'B/' + inJPEGFilename.replace('jpg','xmp')
+    let path3 = '/Volumes/Batholith/Photos/' + tmpSeperatedIPTCDate + '/' + inJPEGFilename.replace('jpg','xmp')
+    let path4 = '/Volumes/Batholith/Photos/photos-' + tmpSeperatedIPTCDate + '/' + inJPEGFilename.replace('jpg','xmp')
+    let path5 = '/Volumes/Batholith/Photos/photos-' + tmpSeperatedIPTCDate + 'B/' + inJPEGFilename.replace('jpg','xmp')
 
     // TODO: need to restore searching for Big\ Ethel/Photos/photos-tmpSeperatedIPTCDate
 
     if (fs.existsSync(path1)) {
-        info = handleXMP(path1, tmpEbirdDate, inJPEGFilename, tmpSeperatedIPTCDate);
+        info = handleXMP(path1, tmpSeperatedIPTCDate, inJPEGFilename);
     } else if (fs.existsSync(path2)) {
-        info = handleXMP(path2, tmpEbirdDate, inJPEGFilename, tmpSeperatedIPTCDate);
+        info = handleXMP(path2, tmpSeperatedIPTCDate, inJPEGFilename);
     } else if (fs.existsSync(path3)) {
-        info = handleXMP(path3, tmpEbirdDate, inJPEGFilename, tmpSeperatedIPTCDate);
+        info = handleXMP(path3, tmpSeperatedIPTCDate, inJPEGFilename);
     } else if (fs.existsSync(path4)) {
-        info = handleXMP(path4, tmpEbirdDate, inJPEGFilename, tmpSeperatedIPTCDate);
+        info = handleXMP(path4, tmpSeperatedIPTCDate, inJPEGFilename);
     } else if (fs.existsSync(path5)) {
-        info = handleXMP(path5, tmpEbirdDate, inJPEGFilename, tmpSeperatedIPTCDate);
+        info = handleXMP(path5, tmpSeperatedIPTCDate, inJPEGFilename);
     } else {
         info.action = 'no XMP for open ' + '/Users/walker/Photography/flickrUP/' + inJPEGFilename
         console.log('\n\n')
@@ -164,7 +171,7 @@ function getStatusForRecentJPEGs() {
     var jpegs = allTheFiles.filter((n) => {
         var stats = fs.statSync('/Users/walker/Photography/flickrUP/' + n);
         var daysOld = (theNow - stats.birthtime)/(24*60*60*1000);
-        return (! stats.isDirectory()) && (daysOld < 40);
+        return (! stats.isDirectory()) && (daysOld < 120);
     });
 
     console.log('recent jpegs', jpegs.length);
