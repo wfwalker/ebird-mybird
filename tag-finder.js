3e23aa2285ab9562a2155cfb084962f564a797b9
Bill Walker <walker@shout.net>
2025-07-30 23:09:56 -0700

    search multiple folders, extract matching entries from ebird taxonomy and sort taxonomically

--- tag-finder.js
+++ tag-finder.js
@@ -4,6 +4,8 @@ const iptc = require('node-iptc');
 const fs = require('fs');
 const XML = require('pixl-xml');
 const path = require('path');
+const papaParse = require('papaparse')
+const ebirdTaxonomy = parseEbirdTaxonony();
 
 /**
  * Recursively searches a directory for files with a specific suffix.
@@ -37,6 +39,24 @@ function findFilesWithSuffix(directoryPath, suffix) {
 }
 
 /**
+ * parse the latest eBird taxononmy file
+ */
+function parseEbirdTaxonony() {
+    let fileBytes = fs.readFileSync('server/data/eBird_taxonomy_v2024.csv', 'utf8')
+
+    if (fileBytes.charCodeAt(0) === 0xFEFF) {
+      fileBytes = fileBytes.slice(1);
+    }
+
+    var ebirdTaxonomy = papaParse.parse(fileBytes, {
+      header: true,
+      dynamicTyping: true
+    })
+
+    return ebirdTaxonomy;
+}
+
+/**
  * Extract the hierarchicalSubject from the XMP and append to the given array
  * @param inXMPPath {string} the path to the XMP file
  * @param inFoundNames {array} the array to accumulate the names we find
@@ -58,15 +78,20 @@ function handleXMP(inXMPPath, inFoundNames) {
     }
 }
 
-// Search through the Photos library for XMP files
-const targetDirectory = '/Volumes/Batholith/Photos/2024/2024-02-20'; // Replace with your desired directory
-const targetSuffix = '.xmp';
-const foundFiles = findFilesWithSuffix(targetDirectory, targetSuffix);
-console.log(`Files with suffix '${targetSuffix}' in '${targetDirectory}':`);
+// Search through the Photos library for XMP files and extract the eBird keywords
+function findEbirdNames(targetDirectory, foundNames) {
+    const targetSuffix = '.xmp';
+    const foundFiles = findFilesWithSuffix(targetDirectory, targetSuffix);
+    console.log(`Files with suffix '${targetSuffix}' in '${targetDirectory}':`);
+
+    foundFiles.forEach(file => handleXMP(file, foundNames));
+}
 
 // Iterate through the XMP files collecting all hierarchicalSubjects
 let foundNames = [];
-foundFiles.forEach(file => handleXMP(file, foundNames));
+findEbirdNames('/Volumes/Batholith/Photos/2025/', foundNames);
+findEbirdNames('/Volumes/Batholith/Photos/2024/', foundNames);
+findEbirdNames('/Volumes/Batholith/Photos/2023/', foundNames);
 
 // Filter the found names for the pipe character that indicates use of eBird taxonomy
 const ebirdNames = foundNames.filter(foundName => foundName.includes("|"));
@@ -74,8 +99,18 @@ const ebirdNames = foundNames.filter(foundName => foundName.includes("|"));
 // Remove duplicates
 uniqueEbirdNames = [...new Set(ebirdNames)];
 
-// All done
-console.log(uniqueEbirdNames);
+// Loop through the unique names and retrieve the matching eBird taxonomy entries
+let taxonomyEntries = []
 
+for (const ebirdName of uniqueEbirdNames) {
+    ebirdNameTokens = ebirdName.split('|');
+    const matchedEntry = ebirdTaxonomy.data.filter(ebT => ebT['PRIMARY_COM_NAME'] == ebirdNameTokens[2]);
+    taxonomyEntries.push(matchedEntry[0]);
+}
+
+// Sort the entries by taxon order
+taxonomyEntries.sort((a, b) => a['TAXON_ORDER'] - b['TAXON_ORDER'])
+
+console.log(taxonomyEntries);
 
 
